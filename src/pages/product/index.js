56c16b0bc7e028f31ee1dc1539bb94a062cfead3
diegoaraujo85 <doasalah@gmail.com>
2020-01-13 02:15:59 -0300

    navegando para o detalhe

--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,7 +1,36 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import api from "../../services/api";
 
-export default class Product extends Component{
-  render(){
-    return <h1>Produto aqui</h1>;
+import './styles.css';
+
+export default class Product extends Component {
+  state = {
+    product: {},
+    goBack:null,
+  };
+
+  async componentDidMount() {
+    const { match:{params:{id}}, history:{goBack} } = this.props;
+
+    const response = await api.get(`/products/${id}`);
+
+    this.setState({ product: response.data, goBack });
+  }
+
+  render() {
+    const { product, goBack } = this.state;
+
+    return (
+      <div className="product-info">
+        <h1>{product.title}</h1>
+        <p>{product.description}</p>
+
+        <p>
+          URL: <a href={product.url}>{product.url}</a>
+        </p>
+
+        <button onClick={goBack}>Voltar</button>
+      </div>
+    );
   }
 }
